f3b6687364dbb402dee45b0b91fb4973921deb76
Matthew Higley <matthewhigley@MattyBook-Pro.local>
2017-01-31 13:46:13 -0800

    updating pages

---
+++ jan30-js-variables/js/script.js
@@ -0,0 +1,28 @@
+// alert('this an alert');
+
+/*
+    comment
+    out
+    multiple
+    lines
+*/
+
+var myNumberVar = 7; // Number Object
+var myFloatVar = 5.2; // Float Object
+var myStringVar = 'Matthew Higley'; // String Object
+var myBooleanVar = true; // Boolean Object
+var myArrayVar = ['one', 'stingray', 'banana', 8, true]; // Array Object
+var myObjectVar = {
+    make: 'Jeep',
+    model: 'Wrangler',
+    color: 'black',
+    year: 2014
+}; // Object Object
+
+
+console.log(typeof myNumberVar);
+console.log(typeof myFloatVar);
+console.log(typeof myStringVar);
+console.log(typeof myBooleanVar);
+console.log(typeof myArrayVar);
+console.log(typeof myObjectVar);
