954e7e0a113cdaa3ae69c44fecab87587fb2b564
Matthew Higley <matthewhigley@MattyBook-Pro.local>
2017-02-01 15:55:13 -0800

    adding js math

---
+++ feb01-js-math/js/script.js
@@ -0,0 +1,76 @@
+/*
+    6 Object Types--
+
+    Object Object
+    Array Object
+    String Object
+    Number Object
+    Float Object
+    Boolean Object
+*/
+
+// Now make 'em, each saved as it's own variable
+
+// When done, console log each object
+
+var strOne = 'this is a string';
+var strTwo = "this is also a string";
+var strThree = 'is this a string";';
+var strFour = 'this is david\'s basketball';
+var strFour = "this is david's basketball";
+var strFive = "Eric said: \"Here have David's Corn nuts\"";
+// Escaping characters
+
+var numOne = 1;
+var numTwo = '2'; // is a string because it's in quotes
+
+var fltOne = 5.7;
+var fltTwo = 0.0000000001;
+
+var boolOne = true;
+var boolTwo = false;
+
+// boolTwo === 0;
+
+var arrOne = ['one', 'two', 'three'];
+var arrTwo = [1, 2, 3];
+var arrTwo = [false, 1, 'two', 3];
+
+var objOne = {
+    one: 'blue',
+    two: 'orange',
+    three: 'pink'
+};
+
+// = is an assignment operator
+// NaN is Not a Number
+
+/*
+    • Create Two new variables
+    • Add the two variables together and assign sum to a new variable
+    • Console result of sum
+*/
+
+var numUno = 22;
+var numDos = 20;
+var equalAdded = (numUno + numDos);
+var equalSubtracted = numUno - numDos;
+var equalMultiplied = numUno * numDos;
+var equalDivided = numUno / numDos;
+var equalModulus = numDos % numUno;
+
+
+var currHairColor;
+var hairColor;
+var hairColor1;
+var HairColor;
+var $hair_color;
+
+var myVariable = 1;
+var myvariable = 2;
+
+// console.log(equalAdded);
+// console.log(equalSubtracted);
+// console.log(equalMultiplied);
+// console.log(equalDivided);
+console.log(equalModulus);
